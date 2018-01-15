db48a2e89df200407b76f24c7ec9a14d74146092
Chris Yang <cyang287@yahoo.com>
2018-01-15 15:11:45 -0500

    add ft-curric-gist-2.js

---
+++ ft-curric-gist-2.js
@@ -0,0 +1,60 @@
+function jediName(firstName, lastName) {
+    let first = firstName.slice(0, 2);
+    let last = lastName.slice(0, 3);
+    console.log(`${last}${first}`);
+}
+jediName("Chris", "Yang");
+
+function beyond(num) {
+    if (isFinite(num) === false) {
+        console.log("And beyond");
+    } else if (num > 0) {
+        console.log("to infinity");
+    } else if (num < 0) {
+        console.log("to negative infinity");
+    } else if (num === 0) {
+        console.log("Staying home.");
+    }
+}
+beyond(0);
+
+function decode(message) {
+    let result = [];
+    let words = message.split(" ");
+    for (i = 0; i < words.length; i++) {
+        if (words[i][0] === "a") {
+            result.push(words[i][1])
+        } else if (words[i][0] === "b") {
+            result.push(words[i][2])
+        } else if (words[i][0] === "c") {
+            result.push(words[i][3])
+        } else if (words[i][0] === "d") {
+            result.push(words[i][4])
+        } else {
+            result.push(" ")
+        };
+    }
+    console.log(result.join(""));
+}
+decode("craft block argon meter bells brown croon droop");
+
+function daysInMonth(month) {
+    switch(month) {
+        case "January":
+        console.log("January has 31 days.")
+        break;
+        case "February":
+        console.log("February has 28 days.")
+        break;
+        case "March":
+        console.log("March has 31 days.")
+        break;
+        case "April":
+        console.log("April has 30 days.")
+        break;
+        case "May":
+        console.log("May has 31 days.")
+        break;
+    }
+}
+daysInMonth("February");
